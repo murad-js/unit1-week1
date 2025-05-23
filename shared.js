13ade61548cd2a0786c112e42d4cf74b4bbfc658
Murad Imanov <imanovmurad896@gmail.com>
2025-05-23 14:17:11 +0300

    refactor: applied prettier rules.

--- shared.js
+++ shared.js
@@ -4,10 +4,10 @@ export const Alignment = Object.freeze({
 });
 
 export const ColumnConfig = {
-  city: {width: 18, align: Alignment.END},
-  population: {width: 10, align: Alignment.START},
-  area: {width: 8, align: Alignment.START},
-  density: {width: 8, align: Alignment.START},
-  country: {width: 18, align: Alignment.START},
-  percentage: {width: 6, align: Alignment.START}
-}
+  city: { width: 18, align: Alignment.END },
+  population: { width: 10, align: Alignment.START },
+  area: { width: 8, align: Alignment.START },
+  density: { width: 8, align: Alignment.START },
+  country: { width: 18, align: Alignment.START },
+  percentage: { width: 6, align: Alignment.START },
+};
